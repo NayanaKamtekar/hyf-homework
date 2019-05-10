18111c58bf3731ec906b3576d9614974bc62f697
NayanaKamtekar <nayana.kamtekar@gmail.com>
2019-05-10 23:11:44 +0200

    Modified homework for Javascript 3 week 3

--- javascript/javascript3/week3/GettingIntoPromises/main.js
+++ javascript/javascript3/week3/GettingIntoPromises/main.js
@@ -1,22 +1,41 @@
-function fetchDataAllAtOnce() {
-    let Promise1 = fetch('https://api.github.com/search/repositories?q=user:ismailirahim')
-        .then(response => response.json())
-    let Promise2 =fetch('https://api.github.com/search/repositories?q=user:Swapna-Sahu')
-        .then(response => response.json())
-    let Promise3 =fetch('https://api.github.com/search/repositories?q=user:hemagunti')
-        .then(response => response.json())
+class GitRepo {
+    constructor(users) {
+        this.users = users;
+    }
 
-    return Promise.all([Promise1, Promise2, Promise3]).then(results => {
-        console.log(results);
-        results.forEach(a =>  {
-            for(let j = 0; j < results.length; j++) {
-                console.log('RepoFullname: ' + a.items[j].name + ', Repo-url: ' 
-                + a.items[j].owner.repos_url + ', RepoOwner: ' + a.items[j].owner.login);
-            } 
+    getUserRepo() {
+        return this.users.map(user => 'https://api.github.com/search/repositories?q=user:' + user).map(url => fetch(url).then(response => response.json()));
+    }
+
+    renderUserRepo() {
+        let gitRepoWrapper = document.getElementsByClassName('gitRepoWrapper')[0];
+        let ul = document.createElement('ul');
+        gitRepoWrapper.appendChild(ul);
+
+        Promise.all(this.getUserRepo())
+        .then(results => {
+            results.forEach(user => {
+                console.log(user);
+                let userLi = document.createElement('li');
+                ul.appendChild(userLi);
+                userLi.innerHTML = user.items[0].owner.login;
+
+                let innerUl = document.createElement('ul');
+                userLi.appendChild(innerUl);
+
+                user.items.forEach(repo => {
+                    let repoLi = document.createElement('li');
+                    innerUl.appendChild(repoLi);
+                    repoLi.innerHTML = repo.name + ': ' + repo.html_url;
+
+                });
+            });
         });
-    });
+    }
 }
-fetchDataAllAtOnce();
+
+const gitRepo = new GitRepo(['ismailirahim', 'Swapna-Sahu', 'hemagunti']);
+gitRepo.renderUserRepo();
 
 /*
 let urls = ['https://api.github.com/search/repositories?q=user:ismailirahim', 'https://api.github.com/search/repositories?q=user:Swapna-Sahu','https://api.github.com/search/repositories?q=user:hemagunti'];
