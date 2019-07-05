e5f7f7f3aa4dc81eede626e3e9fc7e8caf0873ce
NayanaKamtekar <nayana.kamtekar@gmail.com>
2019-07-05 13:31:38 +0200

    Inserted and updated data

--- databases/week2/Homework/homework_library_catalog_data.sql
+++ databases/week2/Homework/homework_library_catalog_data.sql
@@ -85,3 +85,38 @@ insert into loan_transactions (user_id, book_copy_id, loaned_on, due_on, returne
 insert into loan_transactions (user_id, book_copy_id, loaned_on, due_on, returned_on ) values (3, 1, '2019-05-14 08:18:25', '2019-06-14 16:00:00','2019-06-14 11:38:42');
 insert into loan_transactions (user_id, book_copy_id, loaned_on, due_on, returned_on ) values (3, 2, '2019-06-16 11:38:42', '2019-07-16 16:00:00', NULL);
 
+-- modifying purchased_on column on multiple row in book_copies table
+UPDATE book_copies
+SET purchased_on = CASE id 
+WHEN 2 THEN '2009-09-29 10:38:42' 
+WHEN 3 THEN '1999-09-29 12:10:22' 
+WHEN 4 THEN '2007-05-29 10:25:21' 
+WHEN 5 THEN '2019-12-29 14:17:27' 
+WHEN 6 THEN '2003-12-29 11:38:42' 
+WHEN 7 THEN '2016-06-29 13:40:51' 
+WHEN 8 THEN '2014-03-29 10:45:32' 
+ELSE id
+END
+WHERE id IN(2, 3, 4, 5, 6, 7, 8);
+
+-- modifying email and phone column on specific id in the book_copies table
+UPDATE users
+SET email = 'mmeadows@gmail.com', phone = '491-524-6582'
+WHERE id = 6;
+
+-- delete 2 entries  from loan_transactions table
+DELETE FROM loan_transactions
+ORDER BY user_id DESC
+LIMIT 2;
+
+-- removing one column from table
+ALTER TABLE users
+DROP phone;
+
+-- adding one column from in the table
+ALTER TABLE users
+ADD COLUMN address TEXT AFTER phone;
+
+-- adding index on one of the column
+CREATE INDEX users_name
+ON users(name);
